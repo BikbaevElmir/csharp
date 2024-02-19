428cc98a34ef1254a7f0853b100151c17aa5ae99
BikbaevElmir <integral@nxt.ru>
2024-02-19 23:15:50 +0500

    Добавили примеры с 3 урока

---
+++ lesson3/task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая будет принимать на
+//вход два числа и выводить, является ли первое число
+//кратным второму. Если первое число некратно
+//второму, то программа выводит остаток от деления.
+Console.Write("num1: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.Write("num2: ");
+int num2 = int.Parse(Console.ReadLine()!);
+if (num1 % num2 == 0)
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет, " + num1 % num2);
---
+++ lesson3/task4/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая выводит третью с
+//конца цифру заданного числа или сообщает, что
+//третьей цифры нет.
+Console.Write("num: ");
+int num = int.Parse(Console.ReadLine()!);
+if (num < 100)
+    Console.WriteLine("Третьей цифры нет");
+else
+
+    Console.WriteLine((num / 100) % 10);
